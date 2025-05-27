8874b8ff6c7a813daa406494e984eaae450a9619
xxnetvpn <40230501+xxnetvpn@users.noreply.github.com>
2025-05-27 12:53:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ provider "google" {
   zone        = var.myzone
 }
 
-data "google_compute_image" "my_image" {
-  most_recent = true
-  family  = "debian-11"
-  #project = "debian-cloud"
+#data "google_compute_image" "my_image" {
+#  most_recent = true
+#  family  = "debian-11"
+#  #project = "debian-cloud"
   
-}
+#}
 
-output "image_name" {
-  description = "Image Name"
-  value       = data.my_image.name
-}
+#output "image_name" {
+#  description = "Image Name"
+#  value       = data.my_image.name
+#}
