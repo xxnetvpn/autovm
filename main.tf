df7fa0720588c4f785b05b6873fa8fdcb1e62bf0
xxnetvpn <40230501+xxnetvpn@users.noreply.github.com>
2025-05-27 12:42:26 +0800

    Update find image

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
 provider "google" {
-  project     = var.myproject
-  region      = var.myregion
-  zone        = var.myzone
+  project     = var.my_project
+  region      = var.my_region
+  zone        = var.my_zone
+}
+
+data "google_compute_image" "my_image" {
+  most_recent = true
+  family  = "debian-11"
+  #project = "debian-cloud"
+  
+}
+
+output "image_name" {
+  description = "Image Name"
+  value       = data.my_image.name
 }
